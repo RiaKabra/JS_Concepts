271845521b1396083e4df07c82527f63220f170f
RiaKabra <96332121+RiaKabra@users.noreply.github.com>
2024-07-03 16:25:23 +0530

    Create arrays3.js

---
+++ arrays3.js
@@ -0,0 +1,16 @@
+let array = ["Apple","Kiwi","Banana","Orange","Mango","Kiwi"]
+
+let ind_first = array.indexOf("Kiwi")
+let ind_last = array.lastIndexOf("Kiwi")
+
+console.log(ind_first,ind_last)
+
+let array2 = ["Cucumber","Potato"]
+
+let array3 = []
+array3 = array3.concat(array,array2)
+
+console.log(array3)
+
+let sentence = array3.join(" ");
+console.log(sentence);
