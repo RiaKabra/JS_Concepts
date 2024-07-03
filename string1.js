f92fbfdca07fe552a8116d02c00ceb9f263b0b1b
RiaKabra <96332121+RiaKabra@users.noreply.github.com>
2024-07-03 16:42:38 +0530

    Create string1.js

---
+++ string1.js
@@ -0,0 +1,8 @@
+let string = "hello, have a great time!"
+
+console.log(string.length);
+console.log(string.toUpperCase());
+console.log(string.repeat(2));
+
+let pad = string.padStart(30,"#");
+console.log(pad);
